1288e87eeb97118c79aea1ba3ab6f654893452f5
dracen <a.hayashi0113@gmail.com>
2018-05-09 02:33:44 +0900

    PSコントローラ取得を割り込みで実現

--- smart_dana/smart_dana.ino
+++ smart_dana/smart_dana.ino
@@ -1,20 +1,4 @@
-// This example tests nilTimer1.
-
-#include <NilRTOS.h>
-
-// Use tiny unbuffered NilRTOS NilSerial library.
-#include <NilSerial.h>
-
-// Macro to redefine Serial as NilSerial to save RAM.
-// Remove definition to use standard Arduino Serial.
-#define Serial NilSerial
-#include <NilTimer1.h>
-//------------------------------------------------------------------------------
-// Declare a stack with 64 bytes beyond context switch and interrupt needs.
-NIL_WORKING_AREA(waThread1, 64);
-NIL_WORKING_AREA(waThread2, 64);
-
-
+#include <MsTimer2.h>
 
 #include "PS_PAD.h"
 
@@ -25,60 +9,31 @@ NIL_WORKING_AREA(waThread2, 64);
 
 PS_PAD PAD(PS2_SEL);
 
-
 #define RC522_SDA        6  //PD6(SS)
 #define RC522_RESET      5  //PD5(RESET)
 
 
 
-NIL_THREAD(Thread1, arg) {
-  PAD.init();
-  while (TRUE) {
-    nilThdSleep(50);
-
-    int deg;
-    int lead_sw;
-    PAD.poll();
-    deg = PAD.read(PS_PAD::ANALOG_RX);
-    Serial.print(deg);
-    Serial.print("\t");
-    deg = map(deg, -128, 127, 0, 180);
-    Serial.println(deg);
-  }
+void timerFire() {
+  int deg;
+  PAD.poll();
+  deg = PAD.read(PS_PAD::ANALOG_RX);
+  Serial.print(deg);
+  Serial.print("\t");
+  deg = map(deg, -128, 127, 0, 180);
+  Serial.println(deg);
 }
 
-
-NIL_THREAD(Thread2, arg) {
-  while (TRUE) {
-    nilThdSleep(1000);
-    Serial.println("====1s====");
-  }
-}
-
-//------------------------------------------------------------------------------
-/*
-   Threads static table, one entry per thread.  A thread's priority is
-   determined by its position in the table with highest priority first.
-
-   These threads start with a null argument.  A thread's name is also
-   null to save RAM since the name is currently not used.
-*/
-NIL_THREADS_TABLE_BEGIN()
-NIL_THREADS_TABLE_ENTRY(NULL, Thread1, NULL, waThread1, sizeof(waThread1))
-NIL_THREADS_TABLE_ENTRY(NULL, Thread2, NULL, waThread2, sizeof(waThread2))
-NIL_THREADS_TABLE_END()
-//------------------------------------------------------------------------------
 void setup() {
+  PAD.init();
+  MsTimer2::set(100, timerFire);
+  MsTimer2::start();
   pinMode(RC522_SDA, OUTPUT);
   digitalWrite(RC522_SDA, HIGH);
-
   Serial.begin(9600);
-  // start kernel
-  nilSysBegin();
 }
-//------------------------------------------------------------------------------
-// Loop is the idle thread.  The idle thread must not invoke any
-// kernel primitive able to change its state to not runnable.
+
+
 void loop() {
-  // Not used
+
 }
