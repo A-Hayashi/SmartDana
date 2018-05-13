703b18b6de4593bbef2339e0d925c38a22d7d571
dracen <a.hayashi0113@gmail.com>
2018-05-13 22:27:03 +0900

    体重測定できた

--- WeightScale/WeightScale.ino
+++ WeightScale/WeightScale.ino
@@ -1,27 +1,40 @@
-#define LED_PIN 4
-#define SW_PIN 5
+#define PIN D2
 
+#define TIMEOUT_US 30000
 
-void setup() {
-  Serial.begin(9600);
-  pinMode(LED_PIN, OUTPUT);
-  pinMode(SW_PIN, INPUT);
-}
+int read_bits(uint32_t *bh, uint32_t *bl) {
+  int count = 0;
+  uint64_t ret = 0;
 
+  while (true) {
+    unsigned long pulse = pulseIn(PIN, HIGH, TIMEOUT_US);
+    if (pulse == 0) break;
 
-void loop(void) {
-  if(digitalRead(SW_PIN)){
-    Serial.println("aaa");
-    digitalWrite(LED_PIN,HIGH);
-    delay(1000);
-    digitalWrite(LED_PIN,LOW);
-    delay(1000);
-  }else{
-    digitalWrite(LED_PIN,HIGH);
-    delay(200);
-    digitalWrite(LED_PIN,LOW);
-    delay(200);    
+    uint32_t bit = pulse < 750 ? 1 : 0;
+    if (count < 32) {
+      bitWrite(*bh, 31 - count, bit);
+    } else if (count < 64) {
+      bitWrite(*bl, 63 - count, bit);
+    }
+    ++count;
   }
 
+  return count;
 }
 
+void setup() {
+  pinMode(PIN, INPUT);
+
+  Serial.begin(115200);
+}
+
+void loop() {
+  uint32_t bh = 0;
+  uint32_t bl = 0;
+
+  int count = read_bits(&bh, &bl);
+ // Serial.println(count);
+  if (count == 39) {
+    Serial.println(bh & 0xFFFF);
+  }
+}
